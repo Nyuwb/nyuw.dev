ff93bef4cdce943dde6c7a5d09b2864e7cff668e
Nyuw <fabien@nyuw.dev>
2023-01-29 22:45:40 +0000

    Make the sidebar works in mobile display

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,13 @@ document.querySelector('.test').addEventListener('click', e => {
     localStorage.theme = localStorage.theme == 'dark' ? 'light' : 'dark';
     document.documentElement.classList.toggle('dark');
 });
+
+// Sidebar
+document.querySelector('button#display-sidebar').addEventListener('click', e => {
+    e.stopPropagation();
+    document.querySelector('aside').classList.remove('hidden');
+});
+document.querySelector('section#content-wrapper').addEventListener('click', e => {
+    e.stopPropagation();
+    document.querySelector('aside').classList.add('hidden');
+});
