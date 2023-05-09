2fbdf81918b3348ac9ef33de2f48c4141703bf29
Nyuw <fabien@nyuw.dev>
2023-05-09 21:06:44 +0000

    Upgraded Symfony to 6.2.10 and update all dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
