1407e6932b99a11b7095073f7789ec03c27ae4b0
Nyuw <fabien@nyuw.dev>
2024-02-03 11:53:18 +0000

    feat: Upgrade to SF 7.0.3 + removed Webpack Encore integration to now use AssetMapper from SF

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './styles/app.css';
 import './js/app.js';
